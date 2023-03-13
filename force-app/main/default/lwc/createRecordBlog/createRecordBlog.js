b82cf7e9b80793f2b8426eb47cd4fa6853d7bb3e
Balakrishna Ette <balakrishna.ette@outlook.com>
2023-03-14 05:28:37 +0530

    Composition

--- force-app/main/default/lwc/createRecordBlog/createRecordBlog.js
+++ force-app/main/default/lwc/createRecordBlog/createRecordBlog.js
@@ -1,4 +1,7 @@
 import { LightningElement } from 'lwc';
+import EMP_OBJECT from '@salesforce/schema/Employee__c';
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class CreateRecordBlog extends LightningElement {
     formdata = {};
@@ -6,5 +9,37 @@ export default class CreateRecordBlog extends LightningElement {
     changeHandler(event) {
         const {name, value} = event.target;
         this.formdata[name] = value;
+    }
+
+    saveEmployee() {
+        const recordInput = {
+            apiName: EMP_OBJECT.objectApiName,
+            fields: this.formdata
+        };
+        createRecord(recordInput)
+            .then(result => {
+                const successToast = new ShowToastEvent({
+                    title: "Success",
+                    message: "Employee record has been created successfully!",
+                    variant: "success"
+                });
+                this.dispatchEvent(successToast);
+            })
+            .catch(error => {
+                //show error toast
+                console.error(error);
+
+                const successToast = new ShowToastEvent({
+                    title: "Error",
+                    message: error.body.message,
+                    variant: "error"
+                });
+                this.dispatchEvent(successToast);
+            })
+    }
+
+    cancelEmployee() {
+        this.template.querySelector("form.empform").reset();
+        this.formdata = {};
     }
 }
